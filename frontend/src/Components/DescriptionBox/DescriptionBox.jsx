cb93832d76eb8bcd12b1c4224e6c75a2912bfb0c
rashery13@gmail.com <rashery13@gmail.com>
2024-05-08 00:40:59 +0300

    message

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navingator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Step into our boutique and immerse yourself in a world of timeless elegance and contemporary style. Our clothing shop offers a curated collection of fashion-forward pieces that effortlessly blend sophistication with comfort. From luxurious fabrics to exquisite detailing, each garment tells a story of craftsmanship and creativity. As you browse through racks adorned with the latest trends and classic essentials, our attentive staff stands ready to assist, offering personalized styling advice and impeccable service.</p>
+            <p>
+            seeking the perfect ensemble for a special occasion or simply updating your everyday wardrobe, our boutique promises a shopping experience that delights the senses and satisfies the soul.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
